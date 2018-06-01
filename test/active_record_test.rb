0dfb6488607f33df58e114b1e7b53d7e7271a5cb
Ralph Bolo <rbolo062@uottawa.ca>
2018-05-31 22:13:15 -0400

    pass active record test

--- test/active_record_test.rb
+++ test/active_record_test.rb
@@ -1,10 +1,12 @@
 require "test_helper"
 require "active_record"
+require "active_support"
 # require "test_blog/app/models/application_record"
 # require "test_blog/app/models/post"
 
 class ActiveRecordTest < Minitest::Test
   def setup
+    ActiveSupport::Dependencies.autoload_paths = Dir["#{__dir__}/test_blog/app/*"]
     Post.establish_connection(database: "#{__dir__}/test_blog/db/development.sqlite3")
   end
 
