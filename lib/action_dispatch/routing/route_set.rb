1c898b64b025aa5c3ad87a5472d9c39ad5d573fe
Ralph Bolo <rbolo062@uottawa.ca>
2018-06-03 19:21:43 -0400

    implement rails routing dsl

---
+++ lib/action_dispatch/routing/route_set.rb
@@ -0,0 +1,30 @@
+module ActionDispatch
+  module Routing
+    class RouteSet
+      class Route < Struct.new(:method, :path, :controller, :action, :name)
+        def match?(request)
+          request.request_method == method && request.path_info == path
+        end
+      end
+
+      def initialize
+        @routes = []
+      end
+
+      def add_route(*args)
+        route = Route.new(*args)
+        @routes << route
+        route
+      end
+
+      def find_route(request)
+        @routes.detect { |route| route.match?(request) }
+      end
+
+      def draw(&block)
+        mapper = Mapper.new(self)
+        mapper.instance_eval(&block)
+      end
+    end
+  end
+end
